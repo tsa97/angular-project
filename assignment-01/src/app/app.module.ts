3aee0b849a573c9991ed79805b33052d3ac4444d
Tri Sutrisno Adri <37962616+tsa97@users.noreply.github.com>
2019-12-04 08:05:10 +0700

    Assignment-01

---
+++ assignment-01/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningalertComponent,
+    SuccessalertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
